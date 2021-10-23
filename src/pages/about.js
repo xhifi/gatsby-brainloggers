2a46f7ea10aa06901a4dc9b2be61dd0ca7c6efc7
xhifi <shifa.newversion@gmail.com>
2021-10-24 04:38:09 +0500

    another one

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,11 @@
 import React from 'react'
 
 import Layout from '../layouts/Main'
-import Rocket from '../components/Rocket'
 
 const About = () => {
     return (
         <Layout>
-            This is the About Page
-            <div className="ROCKET d-flex align-items-center justify-content-center bg-main-dark">
-                <Rocket />
-            </div>
-            
+            This is the about page
         </Layout>
     )
 }
